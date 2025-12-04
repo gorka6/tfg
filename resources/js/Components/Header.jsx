6376f9d8aacffdc89deeeee9bf16f500052cea6b
gorkaaaaa <659304@alu.murciaeduca.es>
2025-12-04 15:25:52 +0100

    retoques header

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import Dropdown from "@/Components/Framework/Dropdown";
 import { useContextoIdioma } from "@/Contexts/ContextoIdioma";
 import CambioIdioma from "@/Components/CambioIdioma";
@@ -8,7 +8,8 @@ import "../../css/components/header.css";
 export default function Header({ auth }) {
   const { t } = useContextoIdioma();
   const user = auth?.user;
-
+  const { url, component } = usePage();
+  console.log(route("fichas.index"));
   return (
     <header className="header">
       {/* ZONA IZDA: controles */}
@@ -26,7 +27,11 @@ export default function Header({ auth }) {
 
       {/* ZONA CENTRO: fichas - logo - tiradas */}
       <div className="header-center">
-        <Link href={route("fichas.index")} className="header-link header-link--center">
+        <Link
+          href={route("fichas.index")}
+          className={`header-link header-link--center ${url === "/fichas" ? "active" : ""
+            }`}
+        >
           <img src="/images/web/fichas.svg" className="header-icon" alt="icono" />
           {t.home.sheet}
         </Link>
@@ -35,7 +40,11 @@ export default function Header({ auth }) {
           <img src="/images/web/logo.svg" alt="Logo" className="header-logo" />
         </Link>
 
-        <Link href={route("fichas.tiradas")} className="header-link header-link--center">
+        <Link
+          href={route("fichas.tiradas")}
+          className={`header-link header-link--center ${url === "/fichas/tiradas" ? "active" : ""
+            }`}
+        >
           <img src="/images/web/tiradas.svg" className="header-icon" alt="icono" />
           {t.home.throws}
         </Link>
