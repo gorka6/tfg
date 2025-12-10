399270e2e39d6507b5c61f49f84c7b011f4534ee
gorkaaaaa <659304@alu.murciaeduca.es>
2025-12-10 20:15:03 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,29 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
+            // fuerza que la URL de los assets sea relativa
+            // así Laravel + Vite no generará http:// sino https://
+            publicDirectory: 'public',
         }),
-        react(),
     ],
+    server: {
+        // Esto es útil solo para dev local
+        https: true,
+    },
+    build: {
+        // fuerza que Vite use rutas relativas para producción
+        assetsDir: 'build/assets',
+        rollupOptions: {
+            output: {
+                entryFileNames: 'build/assets/[name]-[hash].js',
+                chunkFileNames: 'build/assets/[name]-[hash].js',
+                assetFileNames: 'build/assets/[name]-[hash].[ext]',
+            },
+        },
+    },
 });
