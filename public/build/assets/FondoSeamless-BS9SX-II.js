06213e9d3dd20d5d437b6ade12dc385ab84f4ec8
gorkaaaaa <659304@alu.murciaeduca.es>
2025-12-10 20:18:13 +0100

    Revert "public/build"
    This reverts commit 663fc5a38fb2e1bb81c445469b835041bfaf8d0d.

--- public/build/assets/FondoSeamless-BS9SX-II.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-BHM8Qecn.js";function m({children:s,className:o=""}){return e.jsx("div",{className:`fondo-seamless ${o}`,children:s})}export{m as F};
--- public/build/assets/normTexto-DR_SwMev.js
+++
@@ -1,1 +0,0 @@
-function r(e=""){return e.toString().trim().toLowerCase().replace(/\s+/g,"_").replace(/-/g,"_").replace(/[^\w_]/g,"")}export{r as n};
